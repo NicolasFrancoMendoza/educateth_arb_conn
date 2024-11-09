1f2f9f4dc61013e6fa77839f70a059b957fb5423
nicolasmapache <32186034+NicolasFrancoMendoza@users.noreply.github.com>
2024-11-09 11:37:49 -0500

    conexión completada

--- connection.py
+++ connection.py
@@ -10,7 +10,7 @@ with open("artifacts/Saludo_metadata.json") as f:
 
 ABI = info_json["output"]["abi"] # primer requisito para la conexion
 
-CONTRACT = "0x46792D19FfC3c1eE3381aeEd692E107cFfaa04Ae" # segundo requisito
+CONTRACT = "0x46792D19FfC3c1eE3381aeEd692E107cFfaa04Ae" # segundo requisito, este contrato es el creado con funciones Saludo
 WALLET = os.environ["WALLET"] # tercer requisito
 PRIV_KEY = os.environ["PRIV_KEY"] # cuarto requisito
 
@@ -23,3 +23,36 @@ if w3.is_connected():
     print("Connected to Arbitrum RPC endpoint")
 else:
     print("Connection Failed")
+
+contract_address = CONTRACT
+contract_abi = ABI
+
+contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+account_address = WALLET
+private_key = PRIV_KEY
+
+result = contract.functions.leerSaludo().call() # lectura de función ya creada en contrato ya subido en la blockchain
+
+print("-" * 50)
+print(f"Ultimo saludo: {result}")
+
+
+function_data = contract.functions.guardarSaludo("Hola desde python recargado II").build_transaction({ #estructura de transacción nueva que vamos a hacer
+    'from': account_address,
+    'gas': 5000000,
+    'gasPrice': w3.to_wei('10', 'gwei'),
+    'nonce': w3.eth.get_transaction_count(WALLET),
+    'chainId': 421614, 
+})
+
+signed_transaction = w3.eth.account.sign_transaction(function_data, private_key) #subir la transacción
+transaction_hash = w3.eth.send_raw_transaction(signed_transaction.raw_transaction) #subir el hash
+
+print(f"HASH: {transaction_hash.hex()}")
+
+result2 = contract.functions.leerSaludo().call()
+
+print("-" * 50)
+print(f"Ultimo saludo: {result2}") #imprimir resultado de la función agregada
+
